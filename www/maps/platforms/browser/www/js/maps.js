cde89fde5351460215f23ce6d711e55eea00dc9b
tomasnama <tomasnama@tomasnama-Lenovo-G50-70>
2017-01-29 12:45:35 +0100

    Maps

--- www/maps/platforms/browser/www/js/maps.js
+++ www/maps/platforms/browser/www/js/maps.js
@@ -5,26 +5,36 @@ var app = {
 
     iniciaFastClick: function () {
         FastClick.attach(document.body);
-    }, 
-    
-    dispositivoListo: function() {
+    },
+
+    dispositivoListo: function () {
         navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizacion);
     },
-    
-    pintaCoordenadasEnMapa: function(position){
-    var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18
-    }).addTo(miMapa);
+    pintaCoordenadasEnMapa: function (position) {
+        var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ29vdHlmZXIiLCJhIjoiY2l1MGlrb2M3MDAwMDJ6bXAxY3dlOXdkYiJ9.RBfUsuzHfLrofEyMR8IVlA', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18
+        }).addTo(miMapa);
 
-  },
-    
-    errorAlSolicitarLocalizacion: function(error){
-    console.log(error.code + ': ' + error.message);
-  }
+        app.pintaMarcador([position.coords.latitude, position.coords.longitude], '¡Estoy aquí!', miMapa);
+        
+        miMapa.on('click', function (evento) {
+            var texto = 'Marcador en l(' + evento.latlng.lat.toFixed(2) + ') y L(' + evento.latlng.lng.toFixed(2) + ')';
+            app.pintaMarcador(evento.latlng, texto, miMapa);
+        });
+    },
+
+    pintaMarcador: function (latlng, texto, mapa) {
+        var marcador = L.marker(latlng).addTo(mapa);
+        marcador.bindPopup(texto).openPopup();
+    },
+
+    errorAlSolicitarLocalizacion: function (error) {
+        console.log(error.code + ': ' + error.message);
+    }
 
 };
 
@@ -33,8 +43,8 @@ if ('addEventListener' in document) {
         app.inicio();
         app.dispositivoListo();
     }, false);
-    
-    document.addEventListener('deviceready', function() {
+
+    document.addEventListener('deviceready', function () {
         app.dispositivoListo();
     }, false);
 }
