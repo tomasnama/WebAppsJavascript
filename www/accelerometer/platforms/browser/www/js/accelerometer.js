13677557f4942d7defddd764b4b764c1fca12dbd
tomasnama <tomasnama@tomasnama-Lenovo-G50-70>
2017-02-04 22:48:46 +0100

    Accelerometer

--- www/accelerometer/platforms/browser/www/js/accelerometer.js
+++ www/accelerometer/platforms/browser/www/js/accelerometer.js
@@ -4,17 +4,32 @@ var app = {
         function onError() {
             console.log("Error!");
         }
-
-        navigator.accelerometer.watchAccleration(this.onSuccess, onError, {frequency: 1000});
+        navigator.accelerometer.watchAcceleration(this.onSuccess, onError, {frequency: 1000});
 
     },
 
     onSuccess: function (datosAceleracion) {
+        app.detectaAgitacion(datosAceleracion);
+        app.representaValores(datosAceleracion);
+    },
+
+    detectaAgitacion: function (datosAceleracion) {
+        agitacionX = datosAceleracion.x > 10;
+        agitacionY = datosAceleracion.y > 10;
+
+        if (agitacionX || agitacionY) {
+            document.body.className = 'agitado';
+        } else {
+            document.body.className = '';
+        }
+    },
+
+    representaValores : function (datosAceleracion) {
         app.representa(datosAceleracion.x, "#valorx");
         app.representa(datosAceleracion.y, "#valory");
         app.representa(datosAceleracion.z, "#valorz");
     },
-
+    
     representa: function (dato, elementoHTML) {
         redondeo = Math.round(dato * 100) / 100;
         document.querySelector(elementoHTML).innerHTML = redondeo;
